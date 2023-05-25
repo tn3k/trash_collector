b98127e650fdfc8ac39b471251b4b14324af0ae6
Kent Koehler <kentkoehler10@gmail.com>
2023-05-25 16:10:50 -0400

    marking without the marking part

--- marker.py
+++ marker.py
@@ -4,6 +4,7 @@
 # Date: 5/21/2023
 
 import numpy as np
+import tf
 import cv2 as cv
 from cv_bridge import CvBridge
 import rospy
@@ -28,6 +29,9 @@ MIN_SCAN_ANGLE_RAD = -30.0 / 180 * np.pi
 MAX_SCAN_ANGLE_RAD = +30.0 / 180 * np.pi
 MIN_THRESHOLD_DISTANCE = 0.6    # [m]
 
+# to determine if a position is within another radius
+MARKER_THRESHOLD = 0.5
+
 # Topics and services
 CMD_VEL_TOPIC = 'cmd_vel'
 SCAN_TOPIC = 'scan'
@@ -49,7 +53,8 @@ class fsm(Enum):
 class Marker:
     def __init__(self, gain=GAIN, frequency=FREQUENCY, linear_velocity=LINEAR_VELOCITY, 
                  max_angular_velocity=ANGULAR_VELOCITY_MAX, depth_threshold=DEPTH_THRESHOLD, 
-                 max_color_diff=MAX_COLOR_DIFF, min_region_size=MIN_REGION_SIZE, object_color=OBJECT_COLOR):
+                 max_color_diff=MAX_COLOR_DIFF, min_region_size=MIN_REGION_SIZE, object_color=OBJECT_COLOR,
+                 marker_threshold=MARKER_THRESHOLD):
         """
         Constructor
 
@@ -60,6 +65,9 @@ class Marker:
         # start node in stopped mode
         self._fsm = fsm.EXPLORING
         self.pd = PD(gain[0], gain[1])
+
+        # transformer listener
+        self.listener = tf.TransformListener()
 
         # set parameters
         self.depth_threshold = depth_threshold
@@ -76,14 +84,23 @@ class Marker:
         self.closest_distance = np.inf
         self.close_obstacle = False
 
+        # for camera callbacks
+        self.depth_image = None
+
         # for object detection
         self.max_color_diff = max_color_diff
         self.min_region_size = min_region_size
         self.object_color = object_color
+        self.image_center = 0
         self.bridge = CvBridge()
+        self.regions = []
         self.object = None
-        self.error = None
-        self.depth = None
+        self.error = np.nan
+        self.depth = np.nan
+
+        # for marking
+        self.marked = []
+        self.marker_threshold = marker_threshold
 
         # set up publishers, subscribers, and services
         self._cmd_pub = rospy.Publisher(CMD_VEL_TOPIC, Twist, queue_size=1)
@@ -94,6 +111,10 @@ class Marker:
         # sleep to register
         rospy.sleep(2)  
 
+################################################################################
+# Callbacks
+################################################################################
+
     def _image_callback(self, msg):
         """Finds the regions of matching color"""
         # Initialize
@@ -103,12 +124,13 @@ class Marker:
         image = cv.pyrDown(cv.pyrDown(raw_image))
         height = len(image)
         width = len(image[0])
+        self.image_center = width // 2
 		# Looping over all the pixels in the image
         for y in range(height):
             for x in range(width):
                 pixelColor = image[y, x]
 				# checking if pixel is unvisited and of the correct color
-                if (x,y) not in visited and self.isSimilarColor(pixelColor):
+                if (x,y) not in visited and self.is_similar_color(pixelColor):
                     newRegion = []
                     newRegion.append((x,y))
                     toBeVisited = []
@@ -122,29 +144,77 @@ class Marker:
                         for y2 in range(max(0,curr_pixel[1]-1), min(height-1, curr_pixel[1]+1) + 1):
                             for x2 in range(max(0,curr_pixel[0]-1), min(width-1, curr_pixel[0]+1) + 1):
                                 currentPixelColor = image[y2, x2]
-                                if ((x2,y2) not in visited and self.isSimilarColor(currentPixelColor)):
+                                if ((x2,y2) not in visited and self.is_similar_color(currentPixelColor)):
                                     visited.add((x2,y2))
                                     toBeVisited.append((x2,y2))
 					# If the region is large enough it is added to regions
                     if len(newRegion) >= self.min_region_size:
                         regions.append(newRegion)
         # calculate the largest region    
-        largestRegion = self.largestRegion(regions)
-        self.object = largestRegion
-        # calculate image error
-        self.error = (self.calculateHorizontalError(largestRegion, width / 2) 
-                      if len(largestRegion) > 0 else np.nan)
-        self._error_callback()
+        self.regions = regions
 
-    def calculateHorizontalError(self, region, center_x):
-        total_x = 0
-        for point in region:
-            total_x += point[0]
-        avg_x = total_x / len(region)
+    def _depth_callback(self, msg):
+        raw_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        image = cv.pyrDown(cv.pyrDown(raw_image))
+        self.depth_image = image
+        if self.object is not None:
+            total, size = 0, 0
+            for point in self.object:
+                if not np.isnan(image[point[1], point[0]]):
+                    total += image[point[1], point[0]]
+                    size += 1
+            if size != 0:
+                self.depth = total / size
+            else:
+                self.depth = np.nan
 
-        return center_x - avg_x
+    def _laser_callback(self, msg):
+        if self._fsm == fsm.EXPLORING and not self.close_obstacle:
+            min_index = int(abs(self.min_laser_angle) / msg.angle_increment)
+            max_index = int(abs(self.max_laser_angle) / msg.angle_increment)
+            ranges_in_view = msg.ranges[:min_index] + msg.ranges[-max_index:]
+            # get the minimum distance
+            closest_distance = min(ranges_in_view)
+            if closest_distance < self.laser_threshold:
+                self.close_obstacle = True
 
-    def isSimilarColor(self,color): 
+################################################################################
+################################################################################
+
+    def get_object(self):
+        """
+        Find the biggest region that has not been marked yet
+        """
+        self.regions.sort(reverse=True)   # sort by length with largest at front
+        for region in self.regions:
+            # calculate position
+            if len(region) > self.min_region_size:
+                x = self.calculate_depth(region)
+                # follow region if depth is unknown
+                if np.isnan(x):
+                    return region
+                cam_p = np.array([self.calculate_depth(region), 0, 0, 1])
+                print("camera: {}".format(cam_p))
+                t, r = self.listener.lookupTransform('odom', 'base_link', rospy.Time(0))
+                o_T_c = tf.transformations.translation_matrix(t)
+                o_R_c = tf.transformations.quaternion_matrix(r)
+                o_H_c = o_R_c.dot(o_T_c)
+                odom_p = o_H_c.dot(np.transpose(cam_p))
+                # if position not close to one in marked then return
+                new = True
+                for marker in self.marked:
+                    # find difference from marker
+                    difference = np.array([odom_p[1] - marker[1], odom_p[0] - marker[0]])
+                    # if that difference is less than the threshold then it is not new
+                    if np.linalg.norm(difference) < self.marker_threshold:
+                        new = False
+                # if this is a new point then return the region
+                if new:
+                    return region
+            # if none of the regions are new 
+        return None
+
+    def is_similar_color(self,color): 
         """Determines if two colors are similar given the threshold"""
         # Calculate the total rgb difference
         total_difference = abs(color[0] - self.object_color[0]) + abs(color[1] - self.object_color[1]) + abs(color[2] - self.object_color[2])
@@ -152,52 +222,37 @@ class Marker:
         # otherwise return true
         return not total_difference > self.max_color_diff
 
-    def largestRegion(self, regions):
-        """Finds the largest region in a list of regions"""
-        # Init largest region variable as empty list
-        largest_region = []
-        # Loop through the regions and compare lengths to the current largest region
-        for region in regions:
-            if len(region) > len(largest_region):
-                largest_region = region
-        return largest_region
-
-    def _depth_callback(self, msg):
-        if self._fsm == fsm.FOLLOWING:
-            raw_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            image = cv.pyrDown(cv.pyrDown(raw_image))
-            if self.object is not None and len(self.object) != 0:
-                total, size = 0, 0
-                for point in self.object:
-                    if not np.isnan(image[point[1], point[0]]):
-                        total += image[point[1], point[0]]
-                        size += 1
-                if size != 0:
-                    self.depth = total / size
-                else:
-                    self.depth = None
-            if self.depth is not None and self.depth <= self.depth_threshold:
-                # stop the robot and start the marking process
-                self.stop()
-                self._fsm = fsm.MARKING
-
-    def _error_callback(self):
+    def calculate_pixel_error(self):
         """
-        Parses the error callback to determine whether to drive or not
+        Calculate the error of the average object distance from the center of 
+        the image
         """
-        if self._fsm == fsm.EXPLORING and not np.isnan(self.error):
-            # start following the object
-            self._fsm = fsm.FOLLOWING
-        if self._fsm == fsm.FOLLOWING:
-            if np.isnan(self.error):
-                # start exploration again
-                self.stop()
-                self._fsm = fsm.EXPLORING
+        if self.object is None:
+            return np.nan
+        total_x = 0
+        for point in self.object:
+            total_x += point[0]
+        avg_x = total_x / len(self.object)
+
+        return self.image_center - avg_x
+
+    def calculate_depth(self, obj):
+        """
+        Calculate the depth of an object based on its camera point cluster
+        """
+        if obj is not None and self.depth_image is not None and len(obj) != 0:
+            total, size = 0, 0
+            for point in obj:
+                if not np.isnan(self.depth_image[point[1], point[0]]):
+                    total += self.depth_image[point[1], point[0]]
+                    size += 1
+            if size != 0:
+                return total / size
             else:
-                # calculate the error and proceed to the object
-                self._calculate_error(self.error)
+                return np.nan
+        return np.nan
 
-    def _calculate_error(self, error):
+    def calculate_controller_error(self, error):
         """
         Calculates the error and sends to the PD controller to return an angular
         velocity command
@@ -227,16 +282,6 @@ class Marker:
         # create empty Twist and send to robot if its currently driving
         stop_msg = Twist()
         self._cmd_pub.publish(stop_msg)
-
-    def _laser_callback(self, msg):
-        if self._fsm == fsm.EXPLORING and not self.close_obstacle:
-            min_index = int(abs(self.min_laser_angle) / msg.angle_increment)
-            max_index = int(abs(self.max_laser_angle) / msg.angle_increment)
-            ranges_in_view = msg.ranges[:min_index] + msg.ranges[-max_index:]
-            # get the minimum distance
-            closest_distance = min(ranges_in_view)
-            if closest_distance < self.laser_threshold:
-                self.close_obstacle = True
 
     def random_walk(self):
         """
@@ -270,8 +315,29 @@ class Marker:
                 break
             self.rate.sleep()
 
+    def update_state(self):
+        self.object = self.get_object()
+        self.error = self.calculate_pixel_error()
+        if self._fsm == fsm.EXPLORING and not np.isnan(self.error):
+            # start following the object
+            self._fsm = fsm.FOLLOWING
+        if self._fsm == fsm.FOLLOWING:
+            if np.isnan(self.error):
+                # start exploration again
+                self.stop()
+                self._fsm = fsm.EXPLORING
+            else:
+                # calculate the error and proceed to the object
+                self.depth = self.calculate_depth(self.object)
+                if self.depth < self.depth_threshold:
+                    self.stop()
+                    self._fsm = fsm.MARKING
+                else:
+                    self.calculate_controller_error(self.error)
+
     def spin(self):
         while not rospy.is_shutdown():
+            self.update_state()
             if self._fsm == fsm.FOLLOWING:
                 self.move(self.linear_velocity, self.angular_velocity)
             elif self._fsm == fsm.EXPLORING:
